d97f6ac4cfd53c563f7290fc8b3dac2225f5a1a6
vinicius silva <vinicius silva>
2016-11-24 16:10:20 -0200

    Subindo arquivos sobre variaveis em PHP

---
+++ Variaveis/Variaveis.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<link rel="stylesheet" href="_css/estilo.css">
+	<title> Váriaveis em PHP </title>
+	
+</head>
+<body>
+<div>
+	<?php
+		
+		// Declarando Váriaveis em PHP
+		
+		// Declarando váriveis usando a própria coersão de tipos do PHP
+		$nome = "Gustavo";
+		echo $nome;
+		echo "<br />";
+		
+		//Concatenação 
+		$nome = "Gabrielly Siqueira da Silva";
+		$idade = 03;
+		//Exemplo um
+		echo "$nome tem $idade anos!<br />";
+		
+		//Exemplo dois
+		echo $nome." tem ".$idade." anos!<br />";
+		
+		
+		
+		
+		
+		
+		
+	?>
+</div>
+</body>
+</html>
